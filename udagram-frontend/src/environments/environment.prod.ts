ed32e0e02cf205de2633eb6c703a55704b3330f8
Allen Nick <allennick0991@gmail.com>
2024-07-28 19:30:26 +0700

    feat: deployment k8s

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aa914cdf17db9416dbc2daf71e9fa9e8-1428738568.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
